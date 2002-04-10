c4d7e3894127d1622049d767e1b6e601fb74063c
youngh <youngh>
2002-04-10 21:54:54 +0000

    Initial check-in.

---
+++ H3Matrix4d.java
@@ -0,0 +1,163 @@
+// A limited substitute for javax.vecmath.Matrix4d that uses the MPJava
+// multi-precision floating-point classes.
+//
+// This was independently written without consulting Sun's source, though
+// the interface of this class mimics Sun's.
+
+import mpfun.*;
+
+public final class H3Matrix4d
+{
+    public MPReal m00;
+    public MPReal m01;
+    public MPReal m02;
+    public MPReal m03;
+    public MPReal m10;
+    public MPReal m11;
+    public MPReal m12;
+    public MPReal m13;
+    public MPReal m20;
+    public MPReal m21;
+    public MPReal m22;
+    public MPReal m23;
+    public MPReal m30;
+    public MPReal m31;
+    public MPReal m32;
+    public MPReal m33;
+
+    public H3Matrix4d()
+    {
+	MPReal zero = new MPReal(0);
+
+	this.m00 = zero;  this.m01 = zero;  this.m02 = zero;  this.m03 = zero;
+	this.m10 = zero;  this.m11 = zero;  this.m12 = zero;  this.m13 = zero;
+	this.m20 = zero;  this.m21 = zero;  this.m22 = zero;  this.m23 = zero;
+	this.m30 = zero;  this.m31 = zero;  this.m32 = zero;  this.m33 = zero;
+    }
+
+    public H3Matrix4d(MPReal m00, MPReal m01, MPReal m02, MPReal m03,
+		      MPReal m10, MPReal m11, MPReal m12, MPReal m13,
+		      MPReal m20, MPReal m21, MPReal m22, MPReal m23,
+		      MPReal m30, MPReal m31, MPReal m32, MPReal m33)
+    {
+	this.m00 = m00;  this.m01 = m01;  this.m02 = m02;  this.m03 = m03;
+	this.m10 = m10;  this.m11 = m11;  this.m12 = m12;  this.m13 = m13;
+	this.m20 = m20;  this.m21 = m21;  this.m22 = m22;  this.m23 = m23;
+	this.m30 = m30;  this.m31 = m31;  this.m32 = m32;  this.m33 = m33;
+    }
+
+    public H3Matrix4d(double m00, double m01, double m02, double m03,
+		      double m10, double m11, double m12, double m13,
+		      double m20, double m21, double m22, double m23,
+		      double m30, double m31, double m32, double m33)
+    {
+	this.m00 = new MPReal(m00);  this.m01 = new MPReal(m01);  this.m02 = new MPReal(m02);  this.m03 = new MPReal(m03);
+	this.m10 = new MPReal(m10);  this.m11 = new MPReal(m11);  this.m12 = new MPReal(m12);  this.m13 = new MPReal(m13);
+	this.m20 = new MPReal(m20);  this.m21 = new MPReal(m21);  this.m22 = new MPReal(m22);  this.m23 = new MPReal(m23);
+	this.m30 = new MPReal(m30);  this.m31 = new MPReal(m31);  this.m32 = new MPReal(m32);  this.m33 = new MPReal(m33);
+    }
+
+    public void transform(H3Point4d v)
+    {
+	MPReal x = m00.multiply(v.x).add(m01.multiply(v.y)).add(m02.multiply(v.z)).add(m03.multiply(v.w));
+	MPReal y = m10.multiply(v.x).add(m11.multiply(v.y)).add(m12.multiply(v.z)).add(m13.multiply(v.w));
+	MPReal z = m20.multiply(v.x).add(m21.multiply(v.y)).add(m22.multiply(v.z)).add(m23.multiply(v.w));
+	MPReal w = m30.multiply(v.x).add(m31.multiply(v.y)).add(m32.multiply(v.z)).add(m33.multiply(v.w));
+
+	v.x = x;  v.y = y;  v.z = z;  v.w = w;
+    }
+
+    public void mul(H3Matrix4d m1)
+    {
+	MPReal t00 = m00.multiply(m1.m00).add(m01.multiply(m1.m10)).add(m02.multiply(m1.m20)).add(m03.multiply(m1.m30));
+	MPReal t10 = m10.multiply(m1.m00).add(m11.multiply(m1.m10)).add(m12.multiply(m1.m20)).add(m13.multiply(m1.m30));
+	MPReal t20 = m20.multiply(m1.m00).add(m21.multiply(m1.m10)).add(m22.multiply(m1.m20)).add(m23.multiply(m1.m30));
+	MPReal t30 = m30.multiply(m1.m00).add(m31.multiply(m1.m10)).add(m32.multiply(m1.m20)).add(m33.multiply(m1.m30));
+
+	MPReal t01 = m00.multiply(m1.m01).add(m01.multiply(m1.m11)).add(m02.multiply(m1.m21)).add(m03.multiply(m1.m31));
+	MPReal t11 = m10.multiply(m1.m01).add(m11.multiply(m1.m11)).add(m12.multiply(m1.m21)).add(m13.multiply(m1.m31));
+	MPReal t21 = m20.multiply(m1.m01).add(m21.multiply(m1.m11)).add(m22.multiply(m1.m21)).add(m23.multiply(m1.m31));
+	MPReal t31 = m30.multiply(m1.m01).add(m31.multiply(m1.m11)).add(m32.multiply(m1.m21)).add(m33.multiply(m1.m31));
+
+	MPReal t02 = m00.multiply(m1.m02).add(m01.multiply(m1.m12)).add(m02.multiply(m1.m22)).add(m03.multiply(m1.m32));
+	MPReal t12 = m10.multiply(m1.m02).add(m11.multiply(m1.m12)).add(m12.multiply(m1.m22)).add(m13.multiply(m1.m32));
+	MPReal t22 = m20.multiply(m1.m02).add(m21.multiply(m1.m12)).add(m22.multiply(m1.m22)).add(m23.multiply(m1.m32));
+	MPReal t32 = m30.multiply(m1.m02).add(m31.multiply(m1.m12)).add(m32.multiply(m1.m22)).add(m33.multiply(m1.m32));
+
+	MPReal t03 = m00.multiply(m1.m03).add(m01.multiply(m1.m13)).add(m02.multiply(m1.m23)).add(m03.multiply(m1.m33));
+	MPReal t13 = m10.multiply(m1.m03).add(m11.multiply(m1.m13)).add(m12.multiply(m1.m23)).add(m13.multiply(m1.m33));
+	MPReal t23 = m20.multiply(m1.m03).add(m21.multiply(m1.m13)).add(m22.multiply(m1.m23)).add(m23.multiply(m1.m33));
+	MPReal t33 = m30.multiply(m1.m03).add(m31.multiply(m1.m13)).add(m32.multiply(m1.m23)).add(m33.multiply(m1.m33));
+
+	m00 = t00;  m01 = t01;  m02 = t02;  m03 = t03;
+	m10 = t10;  m11 = t11;  m12 = t12;  m13 = t13;
+	m20 = t20;  m21 = t21;  m22 = t22;  m23 = t23;
+	m30 = t30;  m31 = t31;  m32 = t32;  m33 = t33;
+    }
+
+    public void rotX(double angle)
+    {
+	MPReal zero = new MPReal(0);
+	MPReal one = new MPReal(1);
+	MPReal theta = new MPReal(angle);
+	MPReal cos_theta = theta.cos();
+	MPReal sin_theta = theta.sin();
+	MPReal neg_sin_theta = sin_theta.negate();
+
+	m00 = one;   m01 = zero;       m02 = zero;           m03 = zero;
+	m10 = zero;  m11 = cos_theta;  m12 = neg_sin_theta;  m13 = zero;
+	m20 = zero;  m21 = sin_theta;  m22 = cos_theta;      m23 = zero;
+	m30 = zero;  m31 = zero;       m32 = zero;           m33 = one;
+    }
+
+    public void rotY(double angle)
+    {
+	MPReal zero = new MPReal(0);
+	MPReal one = new MPReal(1);
+	MPReal theta = new MPReal(angle);
+	MPReal cos_theta = theta.cos();
+	MPReal sin_theta = theta.sin();
+	MPReal neg_sin_theta = sin_theta.negate();
+
+	m00 = cos_theta;      m01 = zero;  m02 = sin_theta;  m03 = zero;
+	m10 = zero;           m11 = one;   m12 = zero;       m13 = zero;
+	m20 = neg_sin_theta;  m21 = zero;  m22 = cos_theta;  m23 = zero;
+	m30 = zero;           m31 = zero;  m32 = zero;       m33 = one;
+    }
+
+    public void rotZ(double angle)
+    {
+	MPReal zero = new MPReal(0);
+	MPReal one = new MPReal(1);
+	MPReal theta = new MPReal(angle);
+	MPReal cos_theta = theta.cos();
+	MPReal sin_theta = theta.sin();
+	MPReal neg_sin_theta = sin_theta.negate();
+
+	m00 = cos_theta;  m01 = neg_sin_theta;  m02 = zero;  m03 = zero;
+	m10 = sin_theta;  m11 = cos_theta;      m12 = zero;  m13 = zero;
+	m20 = zero;       m21 = zero;           m22 = one;   m23 = zero;
+	m30 = zero;       m31 = zero;           m32 = zero;  m33 = one;
+    }
+
+    public void setIdentity()
+    {
+	MPReal zero = new MPReal(0);
+	MPReal one = new MPReal(1);
+
+	this.m00 = one;   this.m01 = zero;  this.m02 = zero;  this.m03 = zero;
+	this.m10 = zero;  this.m11 = one;   this.m12 = zero;  this.m13 = zero;
+	this.m20 = zero;  this.m21 = zero;  this.m22 = one;   this.m23 = zero;
+	this.m30 = zero;  this.m31 = zero;  this.m32 = zero;  this.m33 = one;
+    }
+
+    public void print()
+    {
+	System.out.println("--------------------------------------------");
+	System.out.println(m00 + "\t" + m01 + "\t" + m02 + "\t" + m03);
+	System.out.println(m10 + "\t" + m11 + "\t" + m12 + "\t" + m13);
+	System.out.println(m20 + "\t" + m21 + "\t" + m22 + "\t" + m23);
+	System.out.println(m30 + "\t" + m31 + "\t" + m32 + "\t" + m33);
+	System.out.println("--------------------------------------------");
+    }
+}
---
+++ H3Point4d.java
@@ -0,0 +1,117 @@
+// A limited substitute for javax.vecmath.Point4d that uses the MPJava
+// multi-precision floating-point classes.
+//
+// This was independently written without consulting Sun's source, though
+// the interface of this class mimics Sun's.
+
+import mpfun.*;
+
+public final class H3Point4d
+{
+    public MPReal x;
+    public MPReal y;
+    public MPReal z;
+    public MPReal w;
+
+    public H3Point4d()
+    {
+	MPReal zero = new MPReal(0);
+	x = zero;  y = zero;  z = zero;  w = zero;
+    }
+
+    public H3Point4d(H3Point4d rhs)
+    {
+	x = rhs.x;  y = rhs.y;  z = rhs.z;  w = rhs.w;
+    }
+
+    public H3Point4d(MPReal x, MPReal y, MPReal z, MPReal w)
+    {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.w = w;
+    }
+
+    public H3Point4d(double x, double y, double z, double w)
+    {
+	this.x = new MPReal(x);
+	this.y = new MPReal(y);
+	this.z = new MPReal(z);
+	this.w = new MPReal(w);
+    }
+
+    public void scale(MPReal s)
+    {
+	x = x.multiply(s);
+	y = y.multiply(s);
+	z = z.multiply(s);
+	w = w.multiply(s);
+    }
+
+    // this = s*t1 + t2
+    public void scaleAdd(MPReal s, H3Point4d t1, H3Point4d t2)
+    {
+	MPReal tx = t1.x.multiply(s).add(t2.x);
+	MPReal ty = t1.y.multiply(s).add(t2.y);
+	MPReal tz = t1.z.multiply(s).add(t2.z);
+	MPReal tw = t1.w.multiply(s).add(t2.w);
+
+	x = tx;  y = ty;  z = tz;  w = tw;
+    }
+
+    public void project(H3Point4d p1)
+    {
+	MPReal tx = p1.x.divide(p1.w);
+	MPReal ty = p1.y.divide(p1.w);
+	MPReal tz = p1.z.divide(p1.w);
+
+	x = tx;  y = ty;  z = tz;  w = new MPReal(1);
+    }
+
+    public void sub(H3Point4d t1)
+    {
+	x = x.subtract(t1.x);
+	y = y.subtract(t1.y);
+	z = z.subtract(t1.z);
+	w = w.subtract(t1.w);
+    }
+
+    public void set(H3Point4d t1)
+    {
+	x = t1.x;  y = t1.y;  z = t1.z;  w = t1.w;
+    }
+
+    public void set(double x, double y, double z, double w)
+    {
+	this.x = new MPReal(x);
+	this.y = new MPReal(y);
+	this.z = new MPReal(z);
+	this.w = new MPReal(w);
+    }
+
+    // Euclidean norm of homogeneous coordinates [and equivalent to
+    // Point4d.distance(new Point4d(0, 0, 0, 0))].
+    public MPReal vectorLength()
+    {
+	MPReal x2 = x.multiply(x);
+	MPReal y2 = y.multiply(y);
+	MPReal z2 = z.multiply(z);
+	MPReal w2 = w.multiply(w);
+	return x2.add(y2).add(z2).divide(w2).sqrt();
+    }
+
+    // The usual vector dot product.
+    public MPReal vectorDot(H3Point4d v1)
+    {
+	MPReal tx = x.multiply(v1.x);
+	MPReal ty = y.multiply(v1.y);
+	MPReal tz = z.multiply(v1.z);
+	MPReal tw = w.multiply(v1.w);
+	return tx.add(ty).add(tz).add(tw);
+    }
+
+    public void print()
+    {
+	System.out.println("[" + x + ", " + y + ", " + z + ", " + w + "]");
+    }
+}
