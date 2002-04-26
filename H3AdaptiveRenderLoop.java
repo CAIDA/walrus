dfbcf7b9d06a6ba2483350d086dad978e2b5e858
youngh <youngh>
2002-04-26 23:43:59 +0000

    Updated to new name of method H3ViewParameters.installDepthCueing().

--- H3AdaptiveRenderLoop.java
+++ H3AdaptiveRenderLoop.java
@@ -85,7 +85,7 @@ public class H3AdaptiveRenderLoop
 		}
 
 		m_parameters.refresh();
-		m_parameters.installDepthCueing();
+		m_parameters.installBackgroundRelatedAttributes();
 		m_state = STATE_REFRESH;
 	    }
 	}
