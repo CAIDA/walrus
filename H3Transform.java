c352a164f22d80669b64f06a08276975e254e0a0
youngh <youngh>
2002-11-05 00:29:42 +0000

    Replaced buildTranslation(), buildReflection(), and findPivotPoint() with a new independent re-implementation.  I've run Walrus, and the rendering looks correct.  Still missing are new versions of these routines that work with MP values.

--- H3Transform.java
+++ H3Transform.java
@@ -34,6 +34,20 @@
 // is provided by NSF grant ANI-9814421, DARPA NGI Contract N66001-98-2-8922,
 // Sun Microsystems, and CAIDA members.
 // 
+//--------------------------------------------------------------------------
+// Nov 4, 2002
+//
+// The routines buildTranslation(), buildReflection(), and findPivotPoint()
+// have been rewritten from scratch without consulting Jim Donohoe's original
+// code.  The new code is unavoidably very similar to the old since these
+// routines are basically transcriptions of formulas from Phillips and Gunn's
+// paper "Visualizing Hyperbolic Space: Unusual Uses of 4x4 Matrices."  Some
+// comments describing the routines have been preserved.
+//
+// Also, buildEuclideanRotation() has been simply deleted, since it is never
+// used in Walrus.
+//
+//--------------------------------------------------------------------------
 
 import javax.vecmath.*;
 import mpfun.*;
@@ -149,31 +163,6 @@ public class H3Transform
 	return retval;
     }
    
-    public static Matrix4d buildEuclideanRotation(double angle, Vector4d axis)
-    {
-	// Build the matrix for a rotation by the input angle around the axis
-	// of rotation. The notation follows p. 213 of Phillips and Gunn paper.
-	double c = Math.cos( angle );
-	double s = Math.sin( angle );
-	double c1 = 1.0 - c;
-	double u1c1 = axis.x * c1;
-	double u1u2c1 = u1c1 * axis.y;
-	double u1u3c1 = u1c1 * axis.z;
-	double u2u3c1 = axis.y * axis.z * c1;
-	double u1s = axis.x * s;
-	double u2s = axis.y * s;
-	double u3s = axis.z * s;
-	double u1_2 = axis.x * axis.x;
-	double u2_2 = axis.y * axis.y;
-	double u3_2 = axis.z * axis.z;
-
-	return
-	    new Matrix4d( u1_2 + c*(1.0-u1_2), u1u2c1 - u3s, u1u3c1 + u2s, 0.0,
-			  u1u2c1 + u3s, u2_2 + c*(1.0-u2_2), u2u3c1 - u1s, 0.0,
-			  u1u3c1 - u2s, u2u3c1 + u1s, u3_2 + c*(1.0-u3_2), 0.0,
-			  0.0,          0.0,                 0.0,          1.0 );
-    }
-
     public static Matrix4d buildXRotation(double angle)
     {
 	Matrix4d m = new Matrix4d();
@@ -216,343 +205,129 @@ public class H3Transform
 	return m;
     }
 
+    // Build a 4x4 matrix for hyperbolic translation from source point to
+    // dest point.  From p. 212 of Phillips and Gunn paper:
+    //   translate(source,dest)  =  reflect(midpoint) . reflect(source)
     public static Matrix4d buildTranslation(Point4d source, Point4d dest)
     {
-	// Build a 4x4 matrix for hyperbolic translation from source point to
-	// dest point.  From p. 212 of Phillips and Gunn paper:
-	//   translate(source,dest)  =  reflect(midpoint) . reflect(source)
-	//
-	// Midpoint m between hyperbolic points s and d, in homogeneous coords:
-	//    m = SQRT(<d,d>_h * <s,d>_h) * s + SQRT(<s,s>_h * <s,d>_h) * d
-	// where <s,d>_h is the Minkowski inner product:
-	//    <s,d>_h = (s.x*d.x) + (s.y*d.y) + (s.z*d.z) - (s.w*d.w)
+	double aa_h = H3Math.minkowski(source, source);
+	double bb_h = H3Math.minkowski(dest, dest);
+	double ab_h = H3Math.minkowski(source, dest);
+	double sourceScale = Math.sqrt(bb_h * ab_h);
+	double destScale = Math.sqrt(aa_h * ab_h);
+	Point4d midpoint = new Point4d();
+	midpoint.x = sourceScale * source.x + destScale * dest.x;
+	midpoint.y = sourceScale * source.y + destScale * dest.y;
+	midpoint.z = sourceScale * source.z + destScale * dest.z;
+	midpoint.w = sourceScale * source.w + destScale * dest.w;
 
-	// Store some of these in temporaries so they can be used when building
-	// the reflection matrix.
-	double sx2 = source.x * source.x;
-	double sy2 = source.y * source.y;
-	double sz2 = source.z * source.z;
-	double sw2 = source.w * source.w;
-
-	// Calculate Minkowski inner products <s,s>_h, <d,d>_h, and <s,d>_h.
-	double s_s_MIP = sx2 + sy2 + sz2 - sw2;
-	double d_d_MIP = (dest.x * dest.x) + (dest.y * dest.y) +
-	    (dest.z * dest.z) - (dest.w * dest.w);
-	double s_d_MIP = (source.x * dest.x) +
-	    (source.y * dest.y) + (source.z * dest.z) - (source.w * dest.w);
-
-	Point4d midpoint = new Point4d( source );
-      
-	// Scale the source point by SQRT(<d,d>_h * <s,d>_h)
-	midpoint.scale( Math.sqrt( d_d_MIP * s_d_MIP ) );
-
-	// Scale the dest point by SQRT(<s,s>_h * <s,d>_h) the add it to the
-	// scaled source point.
-	midpoint.scaleAdd( Math.sqrt( s_s_MIP * s_d_MIP ), dest, midpoint );
-
-	// Build the reflection matrix for the midpoint.
-	Matrix4d matrix = buildReflection( midpoint );
-
-	// Build the reflection matrix for the source point.
-	Matrix4d reflectSource = buildReflection( source, sx2, sy2,
-							sz2, sw2, s_s_MIP );
-
-	// Multiply the matrix for reflection across the midpoint by the
-	// matrix for reflection across the source.
-	matrix.mul( reflectSource );
-
-	return matrix;
+	Matrix4d r_a = buildReflection(source);
+	Matrix4d r_m = buildReflection(midpoint);
+	r_m.mul(r_a);
+	return r_m;
     }
 
     public static H3Matrix4d buildTranslation(H3Point4d source, H3Point4d dest)
     {
-	// Build a 4x4 matrix for hyperbolic translation from source point to
-	// dest point.  From p. 212 of Phillips and Gunn paper:
-	//   translate(source,dest)  =  reflect(midpoint) . reflect(source)
-	//
-	// Midpoint m between hyperbolic points s and d, in homogeneous coords:
-	//    m = SQRT(<d,d>_h * <s,d>_h) * s + SQRT(<s,s>_h * <s,d>_h) * d
-	// where <s,d>_h is the Minkowski inner product:
-	//    <s,d>_h = (s.x*d.x) + (s.y*d.y) + (s.z*d.z) - (s.w*d.w)
-
-	// Store some of these in temporaries so they can be used when building
-	// the reflection matrix.
-	MPReal sx2 = source.x.multiply(source.x);
-	MPReal sy2 = source.y.multiply(source.y);
-	MPReal sz2 = source.z.multiply(source.z);
-	MPReal sw2 = source.w.multiply(source.w);
-
-	MPReal dx2 = dest.x.multiply(dest.x);
-	MPReal dy2 = dest.y.multiply(dest.y);
-	MPReal dz2 = dest.z.multiply(dest.z);
-	MPReal dw2 = dest.w.multiply(dest.w);
-
-	MPReal sdx = source.x.multiply(dest.x);
-	MPReal sdy = source.y.multiply(dest.y);
-	MPReal sdz = source.z.multiply(dest.z);
-	MPReal sdw = source.w.multiply(dest.w);
-
-	// Calculate Minkowski inner products <s,s>_h, <d,d>_h, and <s,d>_h.
-	MPReal s_s_MIP = sx2.add(sy2).add(sz2).subtract(sw2);
-	MPReal d_d_MIP = dx2.add(dy2).add(dz2).subtract(dw2);
-	MPReal s_d_MIP = sdx.add(sdy).add(sdz).subtract(sdw);
-
-	H3Point4d midpoint = new H3Point4d( source );
-      
-	// Scale the source point by SQRT(<d,d>_h * <s,d>_h)
-	MPReal s_scale = d_d_MIP.multiply(s_d_MIP).sqrt();
-	midpoint.scale(s_scale);
-
-	// Scale the dest point by SQRT(<s,s>_h * <s,d>_h) the add it to the
-	// scaled source point.
-	MPReal d_scale = s_s_MIP.multiply(s_d_MIP).sqrt();
-	midpoint.scaleAdd(d_scale, dest, midpoint );
-
-	// Build the reflection matrix for the midpoint.
-	H3Matrix4d matrix = buildReflection( midpoint );
-
-	// Build the reflection matrix for the source point.
-	H3Matrix4d reflectSource = buildReflection( source, sx2, sy2,
-						    sz2, sw2, s_s_MIP );
-
-	// Multiply the matrix for reflection across the midpoint by the
-	// matrix for reflection across the source.
-	matrix.mul( reflectSource );
-
-	return matrix;
+	return I4_MP;  // XXX: FIX
     }
 
+    // Build a 4x4 matrix for hyperbolic reflection across point p.  From
+    // p. 211 of Phillips and Gunn paper:
+    //       reflect_p = I(4) - 2 * p . pT * I(3,1) / <p,p>_h
+    // where p . pT is the outer product of p with itself, <p,p>_h is the
+    // Minkowski inner product.
     public static Matrix4d buildReflection(Point4d p)
     {
-	// Prepare to build the reflection matrix for the input point.
-	double px2 = p.x * p.x;
-	double py2 = p.y * p.y;
-	double pz2 = p.z * p.z;
-	double pw2 = p.w * p.w;
+	double xx = p.x * p.x;
+	double xy = p.x * p.y;
+	double xz = p.x * p.z;
+	double xw = p.x * p.w;
 
-	// Calculate the Minkowski inner product <p,p>_h
-	double p_p_MIP = px2 + py2 + pz2 - pw2;
+	double yy = p.y * p.y;
+	double yz = p.y * p.z;
+	double yw = p.y * p.w;
 
-	return buildReflection( p, px2, py2, pz2, pw2, p_p_MIP );
+	double zz = p.z * p.z;
+	double zw = p.z * p.w;
+
+	double ww = p.w * p.w;
+
+	Matrix4d ppTI31 = new Matrix4d( xx, xy, xz, -xw,
+					xy, yy, yz, -yw,
+					xz, yz, zz, -zw,
+					xw, yw, zw, -ww);
+
+	double pp_h = xx + yy + zz - ww;
+	ppTI31.mul(-2.0 / pp_h);
+
+	ppTI31.m00 += 1.0;
+	ppTI31.m11 += 1.0;
+	ppTI31.m22 += 1.0;
+	ppTI31.m33 += 1.0;
+
+	return ppTI31;
     }
 
     public static H3Matrix4d buildReflection(H3Point4d p)
     {
-	// Prepare to build the reflection matrix for the input point.
-	MPReal px2 = p.x.multiply(p.x);
-	MPReal py2 = p.y.multiply(p.y);
-	MPReal pz2 = p.z.multiply(p.z);
-	MPReal pw2 = p.w.multiply(p.w);
-
-	// Calculate the Minkowski inner product <p,p>_h
-	MPReal p_p_MIP = px2.add(py2).add(pz2).subtract(pw2);
-
-	return buildReflection( p, px2, py2, pz2, pw2, p_p_MIP );
+	return I4_MP;  // XXX: FIX
     }
 
     //-------------------------------------------------------------------------
     // PRIVATE STATIC METHODS
     //-------------------------------------------------------------------------
 
-    private static Matrix4d buildReflection(Point4d p,
-					    double x2, double y2,
-					    double z2, double w2,
-					    double innerProduct )
-    {
-	// Build a 4x4 matrix for hyperbolic reflection across point p.  From
-	// p. 211 of Phillips and Gunn paper:
-	//       reflect_p = I(4) - 2 * p . pT * I(3,1) / <p,p>_h
-	// where p . pT is the outer product of p with itself, <p,p>_h is the
-	// Minkowski inner product.
-
-	// The Java Vector class doesn't have a builtin outer product, but the
-	// I(3,1) vector has a simple structure so we can construct the outer
-	// product at the same time as we do the multiplication by I(3,1).
-	//
-	//       p . pT           I(3,1)     =   (p . pT) I(3,1)
-	//  -              -   -          -     -               - 
-	// | x2  xy  xz  xw | | 1  0  0  0 |   | x2  xy  xz  -xw |
-	// | yx  y2  yz  yw | | 0  1  0  0 | = | yx  y2  yz  -yw |
-	// | zx  zy  z2  zw | | 0  0  1  0 |   | zx  zy  z2  -zw |
-	// | wx  wy  wz  w2 | | 0  0  0 -1 |   | wx  wy  wz  -w2 |
-	//  -              -   -          -     -               -
-	// The coordinates of point p are real numbers (doubles, actually) so
-	// p_i * p_j = p_j * p_i, so we can save some multiplies.
-	double xy = p.x * p.y;
-	double xz = p.x * p.z;
-	double xw = p.x * p.w;
-	double yz = p.y * p.z;
-	double yw = p.y * p.w;
-	double zw = p.z * p.w;
-	Matrix4d matrix = new Matrix4d( x2, xy, xz, -xw,
-					xy, y2, yz, -yw,
-					xz, yz, z2, -zw,
-					xw, yw, zw, -w2 );
-
-	matrix.mul( -2.0 / innerProduct );
-	matrix.add( I4 );
-
-	return matrix;
-    }
-
-    private static H3Matrix4d buildReflection(H3Point4d p,
-					      MPReal x2, MPReal y2,
-					      MPReal z2, MPReal w2,
-					      MPReal innerProduct )
-    {
-	// Build a 4x4 matrix for hyperbolic reflection across point p.  From
-	// p. 211 of Phillips and Gunn paper:
-	//       reflect_p = I(4) - 2 * p . pT * I(3,1) / <p,p>_h
-	// where p . pT is the outer product of p with itself, <p,p>_h is the
-	// Minkowski inner product.
-
-	// The Java Vector class doesn't have a builtin outer product, but the
-	// I(3,1) vector has a simple structure so we can construct the outer
-	// product at the same time as we do the multiplication by I(3,1).
-	//
-	//       p . pT           I(3,1)     =   (p . pT) I(3,1)
-	//  -              -   -          -     -               - 
-	// | x2  xy  xz  xw | | 1  0  0  0 |   | x2  xy  xz  -xw |
-	// | yx  y2  yz  yw | | 0  1  0  0 | = | yx  y2  yz  -yw |
-	// | zx  zy  z2  zw | | 0  0  1  0 |   | zx  zy  z2  -zw |
-	// | wx  wy  wz  w2 | | 0  0  0 -1 |   | wx  wy  wz  -w2 |
-	//  -              -   -          -     -               -
-	// The coordinates of point p are real numbers (MPReals, actually) so
-	// p_i * p_j = p_j * p_i, so we can save some multiplies.
-	MPReal xy = p.x.multiply(p.y);
-	MPReal xz = p.x.multiply(p.z);
-	MPReal xw = p.x.multiply(p.w);
-	MPReal yz = p.y.multiply(p.z);
-	MPReal yw = p.y.multiply(p.w);
-	MPReal zw = p.z.multiply(p.w);
-	MPReal nxw = xw.negate();
-	MPReal nyw = yw.negate();
-	MPReal nzw = zw.negate();
-	MPReal nw2 = w2.negate();
-
-	MPReal s = new MPReal(-2).divide(innerProduct);
-	MPReal sx2 = x2.multiply(s);
-	MPReal sy2 = y2.multiply(s);
-	MPReal sz2 = z2.multiply(s);
-	MPReal sxy = xy.multiply(s);
-	MPReal sxz = xz.multiply(s);
-	MPReal sxw = xw.multiply(s);
-	MPReal syz = yz.multiply(s);
-	MPReal syw = yw.multiply(s);
-	MPReal szw = zw.multiply(s);
-	MPReal snxw = nxw.multiply(s);
-	MPReal snyw = nyw.multiply(s);
-	MPReal snzw = nzw.multiply(s);
-	MPReal snw2 = nw2.multiply(s);
-
-	MPReal one = new MPReal(1);
-	MPReal isx2 = sx2.add(one);
-	MPReal isy2 = sy2.add(one);
-	MPReal isz2 = sz2.add(one);
-	MPReal isnw2 = snw2.add(one);
-
-	H3Matrix4d matrix = new H3Matrix4d(isx2,  sxy,  sxz,  snxw,
-					    sxy, isy2,  syz,  snyw,
-					    sxz,  syz, isz2,  snzw,
-					    sxw,  syw,  szw, isnw2);
-
-	return matrix;
-    }
-
-
+    // Find the point closest to the origin on the line in H3 passing
+    // through the points a and b. From p. 212 of Phillips and Gunn paper:
+    // 
+    //               a . (a-b)         b . (b-a)
+    //         p = ------------- b + ------------- a
+    //             (a-b) . (a-b)     (b-a) . (b-a)
+    //
+    // --------------------------------------------------------------------
+    // This is computed using affine coordinates and the usual dot product.
+    // --------------------------------------------------------------------
+    //
+    // NOTE: By factoring out a -1, all the (b-a) become (a-b), reducing
+    //       the number of operations.  This also allows us to factor
+    //       out the denominator [(a-b).(a-b)] and make it the new
+    //       homogeneous coordinate w, thus saving three divisions.
+    //
+    //               1
+    //         p = ----- [ (a . d)b - (b . d)a ]
+    //             d . d
+    //
+    //       where d = a - b.  Hence,
+    //
+    //         p_x = (a . d)b_x - (b . d)a_x
+    //         p_y = (a . d)b_y - (b . d)a_y
+    //         p_z = (a . d)b_z - (b . d)a_z
+    //         p_w = d . d
+    //
     private static Point4d findPivotPoint(Point4d a4, Point4d b4)
     {
-	// Find the point closest to the origin on the line in H3 passing
-	// through the points a and b. From p. 212 of Phillips and Gunn paper:
-	// 
-	//               a . (a-b)         b . (b-a)
-	//         p = ------------- b + ------------- a
-	//             (a-b) . (a-b)     (b-a) . (b-a)
-	//
-	// --------------------------------------------------------------------
-	// This is computed using affine coordinates and the usual dot product.
-	// --------------------------------------------------------------------
+	Point3d a = new Point3d();
+	Point3d b = new Point3d();
 
-		// Project from homogenous coordinates to affine coordinates, then create
-		// create vectors from the points so they can be used in the builtin dot
-		// product.
-	Point3d a3 = new Point3d();
-	a3.project( a4 );
-	Vector3d vector_a = new Vector3d( a3 );
-      
-	Point3d b3 = new Point3d();
-	b3.project( b4 );
-	Vector3d vector_b = new Vector3d( b3 );
-      
-	Vector3d a_minus_b = new Vector3d();
-	Vector3d b_minus_a = new Vector3d();
-	a_minus_b.sub( a3, b3 );
-	b_minus_a.sub( b3, a3 );
+	a.project(a4);
+	b.project(b4);
 
-		// The point is constructed in two operations, then at the end result
-		// is scaled by ||a-b||^2, which is (a-b).(a-b), same as (b-a).(b-a).
-		// Scaling is done at the end so we only do one division instead of two.
-	Point3d pivot = new Point3d();
+	Point3d a_minus_b = new Point3d();
+	a_minus_b.sub(a, b);
 
-	// pivot <- (a . (a-b)) * b
-	pivot.scale( vector_a.dot( a_minus_b ), b3 );
+	double p = H3Math.dotProduct(a, a_minus_b);
+	double q = H3Math.dotProduct(b, a_minus_b);
+	double r = H3Math.dotProduct(a_minus_b, a_minus_b);
 
-	// pivot <- (b . (b-a)) * a + (a . (a-b)) * b
-	pivot.scaleAdd( vector_b.dot( b_minus_a ), a3, pivot );
-
-	// Divide result components by ||a-b||^2
-	pivot.scale( 1.0 / a_minus_b.lengthSquared() );
-
-	return new Point4d( pivot.x, pivot.y, pivot.z, 1.0 );
+	return new Point4d(p * b.x - q * a.x,
+			   p * b.y - q * a.y,
+			   p * b.z - q * a.z,
+			   r);
     }
 
     private static H3Point4d findPivotPoint(H3Point4d a4, H3Point4d b4)
     {
-	// Find the point closest to the origin on the line in H3 passing
-	// through the points a and b. From p. 212 of Phillips and Gunn paper:
-	// 
-	//               a . (a-b)         b . (b-a)
-	//         p = ------------- b + ------------- a
-	//             (a-b) . (a-b)     (b-a) . (b-a)
-	//
-	// --------------------------------------------------------------------
-	// This is computed using affine coordinates and the usual dot product.
-	// --------------------------------------------------------------------
-
-	MPReal zero = new MPReal(0);
-
-	H3Point4d a = new H3Point4d(a4);
-	a.project(a);
-	a.w = zero;
-      
-	H3Point4d b = new H3Point4d(b4);
-	b.project(b);
-	b.w = zero;
-      
-	H3Point4d a_minus_b = new H3Point4d(a);
-	a_minus_b.sub(b);
-
-	H3Point4d b_minus_a = new H3Point4d(b);
-	b_minus_a.sub(a);
-
-	MPReal a_dot_a_minus_b = a.vectorDot(a_minus_b);
-	MPReal b_dot_b_minus_a = b.vectorDot(b_minus_a);
-
-	MPReal x1 = a_dot_a_minus_b.multiply(b.x);
-	MPReal y1 = a_dot_a_minus_b.multiply(b.y);
-	MPReal z1 = a_dot_a_minus_b.multiply(b.z);
-
-	MPReal x2 = b_dot_b_minus_a.multiply(a.x);
-	MPReal y2 = b_dot_b_minus_a.multiply(a.y);
-	MPReal z2 = b_dot_b_minus_a.multiply(a.z);
-
-	// ||a-b||^2 == (a-b).(a-b) == (b-a).(b-a)
-	MPReal lengthSquared = a_minus_b.vectorDot(a_minus_b);
-
-	MPReal x = x1.add(x2).divide(lengthSquared);
-	MPReal y = y1.add(y2).divide(lengthSquared);
-	MPReal z = z1.add(z2).divide(lengthSquared);
-
-	return new H3Point4d(x, y, z, new MPReal(1));
+	return ORIGIN4_MP;
     }
 }
