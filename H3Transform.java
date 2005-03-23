d7036a0776c149968a47ff478a412be5c49a2338
youngh <youngh>
2005-03-23 22:37:55 +0000

    Replaced UC license with GPL notice.

--- H3Transform.java
+++ H3Transform.java
@@ -1,51 +1,24 @@
 // 
+// The Walrus Graph Visualization Tool.
 // Copyright (C) 2000,2001,2002 The Regents of the University of California.
-// All Rights Reserved.
 // 
-// Permission to use, copy, modify and distribute any part of this
-// Walrus software package for educational, research and non-profit
-// purposes, without fee, and without a written agreement is hereby
-// granted, provided that the above copyright notice, this paragraph
-// and the following paragraphs appear in all copies.
-//   
-// Those desiring to incorporate this into commercial products or use
-// for commercial purposes should contact the Technology Transfer
-// Office, University of California, San Diego, 9500 Gilman Drive, La
-// Jolla, CA 92093-0910, Ph: (858) 534-5815, FAX: (858) 534-7345.
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 // 
-// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY
-// PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF
-// THE POSSIBILITY OF SUCH DAMAGE.
-//  
-// THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE
-// UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-// SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. THE UNIVERSITY
-// OF CALIFORNIA MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES
-// OF ANY KIND, EITHER IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A
-// PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE
-// ANY PATENT, TRADEMARK OR OTHER RIGHTS.
-//  
-// The Walrus software is developed by the Walrus Team at the
-// University of California, San Diego under the Cooperative Association
-// for Internet Data Analysis (CAIDA) Program.  Support for this effort
-// is provided by NSF grant ANI-9814421, DARPA NGI Contract N66001-98-2-8922,
-// Sun Microsystems, and CAIDA members.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 // 
-// GOVERNMENT PURPOSE RIGHTS
-// Contract No.NGI N66001-98-2-8922
-// Contractor Name: SPAWAR
-// Expiration Date: 3/1/2008
-// The Government's rights to use, modify, reproduce, release, perform, 
-// display, or disclose these technical data are restricted by paragraph 
-// (b)(2) of the Rights in Technical Data - Noncommercial Items clause 
-// contained in the above identified contract.  No restrictions apply after 
-// the expiration date shown above.  Any reproduction of technical data or 
-// portions thereof marked with this legend must also reproduce the markings.
-//
-//######END_HEADER######
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ######END_HEADER######
+// 
+
 
 //--------------------------------------------------------------------------
 // Nov 4, 2002
