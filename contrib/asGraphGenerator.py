08da38d752b58045c8cad4f4f1c662511af8487e
dennmar <dennismariano3@gmail.com>
2017-10-24 14:35:20 -0700

    Update asGraphGenerator.py to reduce code redundancy and increase readability

--- contrib/asGraphGenerator.py
+++ contrib/asGraphGenerator.py
@@ -118,6 +118,110 @@ def find(list, asn, is_list_of_tuples, low, high):
   else:
     return -1
 
+def insertRelationship(autonomous_systems, rel_type, asn1, asn2):
+  """
+  Inserts an ASN into an Autonomous System's list of providers or list of
+  siblings.
+
+  Args:
+    autonomous_systems (List): The list containing all autonomous systems.
+    rel_type (int): -1 if there is a provider-customer relationship between
+                    the first AS and the second AS, 1 if there is a customer-
+                    provider relationship, 0 if there is a sibling-sibling
+                    relationship.
+    asn1 (int): The Autonomous System Number of the first autonomous system
+                in the relationship.
+    asn2 (int): The Autonomous System Number of the second autonomous system
+                in the relationship.
+  """
+  # insert the first autonomous system in the list of providers for the
+  # second autonomous system if they possess a provider-customer
+  # relationship
+  if rel_type == -1:
+    insertProvider(autonomous_systems, asn1, asn2)
+
+  # insert the second autonomous system in the list of providers for the
+  # first autonomous system if they possess a customer-provider
+  # relationship
+  if rel_type == 1:
+    insertProvider(autonomous_systems, asn2, asn1)
+
+  # insert the first autonomous system in the list of siblings for the
+  # second autonomous system listed if they possess a sibling-sibling
+  # relationship
+  if rel_type == 0:
+    insertSibling(autonomous_systems, asn1, asn2)
+
+def insertProvider(autonomous_systems, provider, customer):
+  """
+  Inserts an ASN into an Autonomous System's list of providers.
+
+  Args:
+    autonomous_systems (List): The list containing all autonomous systems.
+    provider (int): The ASN of the provider.
+    customer (int): The ASN of the customer.
+  """
+  as_index = find(autonomous_systems, customer, True, 0,
+      len(autonomous_systems) - 1)
+  as_providers = autonomous_systems[as_index][1]
+  sortedInsert(as_providers, provider, False, 0, len(as_providers) - 1)
+
+def insertSibling(autonomous_systems, sibling1, sibling2):
+  """
+  Inserts an ASN into an Autonomous System's list of siblings.
+
+  Args:
+    autonomous_systems (List): The list containing all autonomous systems.
+    sibling1 (int): The ASN of an Autonomous System in a sibling-sibling
+                    relationship.
+    sibling2 (int): The ASN of an Autonomous System in a sibling-sibling
+                    relationship, assumed to be different from sibling1.
+  """
+  as_index = find(autonomous_systems, sibling2, True, 0,
+        len(autonomous_systems) - 1)
+  as_siblings = autonomous_systems[as_index][2]
+  sortedInsert(as_siblings, sibling1, False, 0, len(as_siblings) - 1)
+
+def parseRelationships(file):
+  """
+  Creates a list of all relationships for all Autonomous Systems from
+  the given file.
+
+  Args:
+    file (File): The file containing the provider-customer, customer-
+                 provider, and sibling-sibling relationships for
+                 Autonomous Systems.
+
+  Returns:
+    A list of tuples where each tuple contains three elements: the ASN
+    of an Autonomous System, a list of that Autonomous System's providers,
+    and a list of that Autonomous System's siblings.
+  """
+  autonomous_systems = []
+
+  rel_lines = file.readlines()
+  for i in range (0, len(rel_lines)):
+    if rel_lines[i].find("#") == -1:
+      rel = rel_lines[i].strip().split("|")
+      asn1 = int(rel[0])
+      asn2 = int(rel[1])
+      rel_type = int(rel[2])
+
+      rel_tuple = (asn1, [], [])
+      sortedInsert(autonomous_systems, rel_tuple, True, 0,
+          len(autonomous_systems) - 1)
+
+      rel_tuple = (asn2, [], [])
+      sortedInsert(autonomous_systems, rel_tuple, True, 0,
+          len(autonomous_systems) - 1)
+
+      insertRelationship(autonomous_systems, rel_type, asn1, asn2)
+
+  for i in range(0, len(autonomous_systems)):
+    print autonomous_systems[i]
+
+  return autonomous_systems
+
 def writeMetadataSection(file, c_args):
   """
   Writes the basic information of the graph to the file.
@@ -151,52 +255,7 @@ def writeStructuralDataSection(file, c_args):
   relationships_file = open(c_args.r, "r")
   clique_file = open(c_args.c, "r")
 
-  autonomous_systems = []
-
-  rel_lines = relationships_file.readlines()
-  for i in range (0, len(rel_lines)):
-    if rel_lines[i].find("#") == -1:
-      rel = rel_lines[i].strip().split("|")
-      asn1 = int(rel[0])
-      asn2 = int(rel[1])
-
-      rel_tuple = (asn1, [], [])
-      sortedInsert(autonomous_systems, rel_tuple, True, 0,
-          len(autonomous_systems) - 1)
-
-      rel_tuple = (asn2, [], [])
-      sortedInsert(autonomous_systems, rel_tuple, True, 0,
-          len(autonomous_systems) - 1)
-
-      # insert the first autonomous system in the list of providers for the
-      # second autonomous system if they possess a provider-customer
-      # relationship
-      if rel[2] == '-1':
-        as_index = find(autonomous_systems, asn2, True, 0,
-            len(autonomous_systems) - 1)
-        as_providers = autonomous_systems[as_index][1]
-        sortedInsert(as_providers, asn1, False, 0, len(as_providers) - 1)
-
-      # insert the second autonomous system in the list of providers for the
-      # first autonomous system if they possess a customer-provider
-      # relationship
-      if rel[2] == '1':
-        as_index = find(autonomous_systems, asn1, True, 0,
-            len(autonomous_systems) - 1)
-        as_providers = autonomous_systems[as_index][1]
-        sortedInsert(as_providers, asn2, False, 0, len(as_providers) - 1)
-
-      # insert the first autonomous system in the list of siblings for the
-      # second autonomos system listed if they possess a sibling-sibling
-      # relationship
-      if rel[2] == '0':
-        as_index = find(autonomous_systems, asn2, True, 0,
-            len(autonomous_systems) - 1)
-        as_siblings = autonomous_systems[as_index][2]
-        sortedInsert(as_siblings, asn1, False, 0, len(as_siblings) - 1)
-
-  for i in range(0, len(autonomous_systems)):
-    print autonomous_systems[i]
+  parseRelationships(relationships_file)
 
 def main():
   """
